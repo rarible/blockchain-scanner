42abd3ee8e20901fb50a9026df135066f64e269f
Artem Yakischik <a.yakischik@rarible.com>
2021-08-02 12:44:49 +0400

    Logging context fix

--- core/src/main/kotlin/com/rarible/blockchain/scanner/BlockEventListener.kt
+++ core/src/main/kotlin/com/rarible/blockchain/scanner/BlockEventListener.kt
@@ -14,13 +14,13 @@ import com.rarible.blockchain.scanner.framework.service.PendingLogService
 import com.rarible.blockchain.scanner.subscriber.LogEventListener
 import com.rarible.blockchain.scanner.subscriber.LogEventPostProcessor
 import com.rarible.blockchain.scanner.subscriber.LogEventSubscriber
+import com.rarible.core.logging.RaribleMDCContext
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toCollection
-import kotlinx.coroutines.slf4j.MDCContext
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeout
 import org.slf4j.LoggerFactory
@@ -53,7 +53,7 @@ class BlockEventListener<BB : BlockchainBlock, BL : BlockchainLog, B : Block, L 
         logger.info("Received BlockEvent [{}]", event)
         event.contextParams.forEach { (key, value) -> MDC.put(key, value) }
 
-        withContext(MDCContext()) {
+        withContext(RaribleMDCContext()) {
             val logs = processBlock(event)
             val status = postProcessBlock(event, logs)
             updateBlockStatus(event, status)
@@ -71,6 +71,7 @@ class BlockEventListener<BB : BlockchainBlock, BL : BlockchainLog, B : Block, L 
         val status = try {
             logger.debug(
                 "Starting to post-process {} Logs of BlockEvent [{}] for {} post-processors",
+                // TODO оставить только эти слушатели
                 logs.size, event, logEventPostProcessors.size
             )
 
