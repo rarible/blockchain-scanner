a2c0c6953bc914f07701e1bd5258e4560b5ccdba
zolt <timochkinea@gmail.com>
2021-11-09 16:53:07 +0700

    fix start block number calculation

--- flow/src/main/kotlin/com/rarible/blockchain/scanner/flow/reconciliation/FlowReconciliationFromProvider.kt
+++ flow/src/main/kotlin/com/rarible/blockchain/scanner/flow/reconciliation/FlowReconciliationFromProvider.kt
@@ -1,6 +1,7 @@
 package com.rarible.blockchain.scanner.flow.reconciliation
 
 import com.nftco.flow.sdk.FlowChainId
+import com.rarible.blockchain.scanner.flow.service.SporkService
 import com.rarible.blockchain.scanner.flow.subscriber.FlowLogEventSubscriber
 import com.rarible.blockchain.scanner.reconciliation.ReconciliationFromProvider
 import org.springframework.beans.factory.annotation.Value
@@ -10,14 +11,14 @@ import org.springframework.stereotype.Component
 class FlowReconciliationFromProvider(
     @Value("\${blockchain.scanner.flow.chainId}")
     private val chainId: FlowChainId,
-    private val subscribers: List<FlowLogEventSubscriber>
+    private val subscribers: List<FlowLogEventSubscriber>,
+    private val sporkService: SporkService
 ) : ReconciliationFromProvider {
 
     override fun initialFrom(descriptorId: String): Long {
         val sub = subscribers.first { descriptorId == it.getDescriptor().id }
         return sub.getDescriptor().startFrom ?: when(chainId) {
-            FlowChainId.MAINNET -> 7601063L
-            FlowChainId.TESTNET -> 47330085L
+            FlowChainId.MAINNET, FlowChainId.TESTNET -> sporkService.allSporks[chainId]!!.last().from
             FlowChainId.EMULATOR -> 0L
             else -> throw IllegalArgumentException("Unsupported chain-id : $chainId")
         }
