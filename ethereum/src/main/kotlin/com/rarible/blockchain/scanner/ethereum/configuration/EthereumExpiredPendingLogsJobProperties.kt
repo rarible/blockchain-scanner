52dfc44565657fdf8d19b13dd634a3f6090c904d
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-12-21 10:08:21 +0300

    fix: cleanup unused class.

--- ethereum/src/main/kotlin/com/rarible/blockchain/scanner/ethereum/configuration/EthereumExpiredPendingLogsJobProperties.kt
+++
@@ -1,9 +0,0 @@
-package com.rarible.blockchain.scanner.ethereum.configuration
-
-import java.time.Duration
-
-data class EthereumExpiredPendingLogsJobProperties(
-    val interval: Duration = Duration.ofHours(1),
-    val initialDelay: Duration = Duration.ofMinutes(1),
-    val maxPendingLogDuration: Long = Duration.ofHours(2).toMillis()
-)
