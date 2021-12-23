c1acdfc747cd626dd897f6f78e7d3ba0a68109a3
Mikhail Nasimov <m.nasimov@rarible.com>
2021-12-23 16:09:27 +0300

    fix solana client & bump version

--- solana/src/main/kotlin/com/rarible/blockchain/scanner/solana/client/SolanaClient.kt
+++ solana/src/main/kotlin/com/rarible/blockchain/scanner/solana/client/SolanaClient.kt
@@ -4,13 +4,18 @@ import com.rarible.blockchain.scanner.framework.client.BlockchainClient
 import com.rarible.blockchain.scanner.framework.data.FullBlock
 import com.rarible.blockchain.scanner.solana.client.dto.GetBlockRequest.TransactionDetails
 import com.rarible.blockchain.scanner.solana.client.dto.toModel
+import com.rarible.blockchain.scanner.solana.configuration.SolanaBlockchainScannerProperties
 import com.rarible.blockchain.scanner.solana.model.SolanaDescriptor
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import org.springframework.stereotype.Component
 
-class SolanaClient(url: String) : BlockchainClient<SolanaBlockchainBlock, SolanaBlockchainLog, SolanaDescriptor> {
-    private val api = SolanaHttpRpcApi(url)
+@Component
+class SolanaClient(
+    properties: SolanaBlockchainScannerProperties
+) : BlockchainClient<SolanaBlockchainBlock, SolanaBlockchainLog, SolanaDescriptor> {
+    private val api = SolanaHttpRpcApi(properties.rpcApiUrl)
 
     override val newBlocks: Flow<SolanaBlockchainBlock>
         get() = flow {
