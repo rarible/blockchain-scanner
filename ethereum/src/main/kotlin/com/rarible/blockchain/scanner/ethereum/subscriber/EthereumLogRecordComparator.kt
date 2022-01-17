ff897e15800ee66b30f76373e92be380708c2095
sanufriev <s.anufriev@rarible.com>
2022-01-17 10:08:09 +0700

    Releae 2.1.32. fix ethereum record comparator

--- ethereum/src/main/kotlin/com/rarible/blockchain/scanner/ethereum/subscriber/EthereumLogRecordComparator.kt
+++ ethereum/src/main/kotlin/com/rarible/blockchain/scanner/ethereum/subscriber/EthereumLogRecordComparator.kt
@@ -7,18 +7,22 @@ import com.rarible.blockchain.scanner.framework.subscriber.LogRecordComparator
 object EthereumLogRecordComparator : LogRecordComparator<EthereumLogRecord> {
     override fun compare(r1: EthereumLogRecord, r2: EthereumLogRecord): Int {
         return when {
-            r1.log.status == EthereumLogStatus.PENDING && r2.log.status == EthereumLogStatus.PENDING ->
+            r1.log.status.isPending() && r2.log.status.isPending() ->
                 compareBy<EthereumLogRecord> { it.log.transactionHash }
                     .thenBy { it.log.address.toString() }
                     .thenBy { it.log.topic.toString() }
                     .thenBy { it.log.minorLogIndex }
                     .compare(r1, r2)
-            r1.log.status == EthereumLogStatus.PENDING -> -1
-            r2.log.status == EthereumLogStatus.PENDING -> 1
+            r1.log.status.isPending() -> -1
+            r2.log.status.isPending() -> 1
             else -> compareBy<EthereumLogRecord> { it.log.blockNumber!! }
                 .thenBy { it.log.logIndex!! }
                 .thenBy { it.log.minorLogIndex }
                 .compare(r1, r2)
         }
+    }
+
+    private fun EthereumLogStatus.isPending(): Boolean {
+        return this == EthereumLogStatus.PENDING || this == EthereumLogStatus.DROPPED || this == EthereumLogStatus.INACTIVE
     }
 }
