e75ca410069fe0189f2028e8112560474ce1ce8d
Mikhail Nasimov <m.nasimov@rarible.com>
2022-01-18 06:24:26 +0300

    amend solana log record

--- solana/src/main/kotlin/com/rarible/blockchain/scanner/solana/model/SolanaLogRecord.kt
+++ solana/src/main/kotlin/com/rarible/blockchain/scanner/solana/model/SolanaLogRecord.kt
@@ -2,6 +2,6 @@ package com.rarible.blockchain.scanner.solana.model
 
 import com.rarible.blockchain.scanner.framework.model.LogRecord
 
-abstract class SolanaLogRecord(
-    val log: SolanaLog
-) : LogRecord
+abstract class SolanaLogRecord : LogRecord {
+    abstract val log: SolanaLog
+}
