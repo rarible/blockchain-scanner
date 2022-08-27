c1cab1c705eb0134be6f0a48bef82569b4167f4a
sanufriev <s.anufriev@rarible.com>
2022-08-27 22:59:11 +0700

    feat: Fix StackOverflowError in logback, it cause scaner stop

--- core/src/main/kotlin/com/rarible/blockchain/scanner/handler/BlockHandlerException.kt
+++ core/src/main/kotlin/com/rarible/blockchain/scanner/handler/BlockHandlerException.kt
@@ -2,10 +2,6 @@ package com.rarible.blockchain.scanner.handler
 
 import org.slf4j.LoggerFactory
 
-class BlockHandlerException : RuntimeException {
-    constructor(message: String, cause: Throwable): super(message, cause)
-}
-
 private val logger = LoggerFactory.getLogger("BlockHandlerException")
 
 suspend fun <T> runRethrowingBlockHandlerException(actionName: String, block: suspend () -> T): T {
@@ -13,9 +9,6 @@ suspend fun <T> runRethrowingBlockHandlerException(actionName: String, block: su
         block()
     } catch (e: Exception) {
         logger.error("Failed to '$actionName'", e)
-        if (e is BlockHandlerException) {
-            throw e
-        }
-        throw BlockHandlerException("Failed to '$actionName'", e)
+        throw e
     }
 }
