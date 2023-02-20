4bad1e72ea732111fdc1c4e4a393d04f0da8a074
sanufriev <s.anufriev@rarible.com>
2023-02-20 15:17:03 +0700

    feat: PT-2187 Add EnableBlockchainScannerComponents annotation

---
+++ core/src/main/kotlin/com/rarible/blockchain/scanner/EnableBlockchainScannerComponents.kt
@@ -0,0 +1,11 @@
+package com.rarible.blockchain.scanner
+
+import com.rarible.blockchain.scanner.block.BlockRepository
+import org.springframework.context.annotation.ComponentScan
+
+@Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@ComponentScan(basePackageClasses = [
+    BlockRepository::class
+])
+annotation class EnableBlockchainScannerComponents
