a99aa6b4ca5507248600ff6a5f14f3be3c7cc3df
Dmitry Saraykin <d.saraykin@rarible.com>
2023-06-26 14:59:09 +0300

    feat(PT-2987): set-base-uri event

--- framework/src/main/kotlin/com/rarible/blockchain/scanner/framework/model/TransactionRecord.kt
+++ framework/src/main/kotlin/com/rarible/blockchain/scanner/framework/model/TransactionRecord.kt
@@ -1,3 +1,6 @@
 package com.rarible.blockchain.scanner.framework.model
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
 interface TransactionRecord : Record
