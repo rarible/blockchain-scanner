f3273b9c12d4148456d75964c5680a8f2c54d3de
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2023-07-20 13:48:11 +0400

    fix: Retries for Eth moved to MonoEthereum (#101)

--- core/src/main/kotlin/com/rarible/blockchain/scanner/configuration/ClientRetryPolicyProperties.kt
+++ core/src/main/kotlin/com/rarible/blockchain/scanner/configuration/ClientRetryPolicyProperties.kt
@@ -2,7 +2,7 @@ package com.rarible.blockchain.scanner.configuration
 
 import java.time.Duration
 
-class ClientRetryPolicyProperties(
+data class ClientRetryPolicyProperties(
     val delay: Duration = Duration.ofMillis(125),
     val increment: Duration = Duration.ofMillis(125),
     val attempts: Int = 8,
