8e2ffec6fe655ef8620663cc149a402c82136d41
Dmitry Saraykin <d.saraykin@rarible.com>
2023-10-03 15:17:50 +0300

    fix(PT-3899): flow timeout

--- flow/src/main/kotlin/com/rarible/blockchain/scanner/flow/service/FlowApiFactoryImpl.kt
+++ flow/src/main/kotlin/com/rarible/blockchain/scanner/flow/service/FlowApiFactoryImpl.kt
@@ -3,13 +3,19 @@ package com.rarible.blockchain.scanner.flow.service
 import com.nftco.flow.sdk.Flow
 import com.rarible.blockchain.scanner.flow.configuration.FlowBlockchainScannerProperties
 import com.rarible.blockchain.scanner.monitoring.BlockchainMonitor
+import io.grpc.CallOptions
+import io.grpc.Channel
+import io.grpc.ClientCall
+import io.grpc.ClientInterceptor
 import io.grpc.HttpConnectProxiedSocketAddress
 import io.grpc.ManagedChannelBuilder
+import io.grpc.MethodDescriptor
 import io.grpc.ProxyDetector
 import org.onflow.protobuf.access.AccessAPIGrpc
 import org.springframework.stereotype.Component
 import java.net.InetSocketAddress
 import java.net.URI
+import java.util.concurrent.TimeUnit
 
 @Component
 class FlowApiFactoryImpl(
@@ -37,6 +43,21 @@ class FlowApiFactoryImpl(
             .run { if (detector != null) proxyDetector(detector) else this }
             .usePlaintext()
             .userAgent(Flow.DEFAULT_USER_AGENT)
+            .intercept(object : ClientInterceptor {
+                override fun <ReqT : Any?, RespT : Any?> interceptCall(
+                    descriptor: MethodDescriptor<ReqT, RespT>?,
+                    options: CallOptions,
+                    channel: Channel
+                ): ClientCall<ReqT, RespT> {
+                    return channel.newCall(
+                        descriptor,
+                        options.withDeadlineAfter(
+                            flowBlockchainScannerProperties.timeout.toMillis(),
+                            TimeUnit.MILLISECONDS
+                        )
+                    )
+                }
+            })
             .build()
         val api = AsyncFlowAccessApiImpl(AccessAPIGrpc.newFutureStub(channel))
         return MonitoredFlowApi(delegate = api, closeable = api, blockchainMonitor = blockchainMonitor)
