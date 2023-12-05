de42e28f2b3b09fb54237e8f34ae8e0e02d2166e
Roman Novikov <mystdeim@gmail.com>
2023-12-05 10:34:42 +0000

    feat: polling by default

--- ethereum/src/main/kotlin/com/rarible/blockchain/scanner/ethereum/configuration/BlockPollerProperties.kt
+++ ethereum/src/main/kotlin/com/rarible/blockchain/scanner/ethereum/configuration/BlockPollerProperties.kt
@@ -3,6 +3,6 @@ package com.rarible.blockchain.scanner.ethereum.configuration
 import java.time.Duration
 
 data class BlockPollerProperties(
-    val enabled: Boolean = false,
+    val enabled: Boolean = true,
     val period: Duration = Duration.ofSeconds(1)
 )
