53614ba7c2fa88771239653579ab3121f15452b0
sanufriev <s.anufriev@rarible.com>
2024-01-25 13:33:15 +0700

    feat: PT-4676 Add test

--- ethereum/src/test/kotlin/com/rarible/blockchain/scanner/ethereum/client/EthereumClientTest.kt
+++ ethereum/src/test/kotlin/com/rarible/blockchain/scanner/ethereum/client/EthereumClientTest.kt
@@ -5,8 +5,12 @@ import com.rarible.blockchain.scanner.configuration.RetryPolicyProperties
 import com.rarible.blockchain.scanner.configuration.ScanProperties
 import com.rarible.blockchain.scanner.ethereum.configuration.BlockPollerProperties
 import com.rarible.blockchain.scanner.ethereum.configuration.EthereumScannerProperties
+import com.rarible.blockchain.scanner.ethereum.model.EthereumDescriptor
+import com.rarible.blockchain.scanner.ethereum.reduce.createRandomEthereumLog
 import com.rarible.blockchain.scanner.ethereum.test.data.ethBlock
+import com.rarible.blockchain.scanner.ethereum.test.data.randomAddress
 import com.rarible.blockchain.scanner.ethereum.test.data.randomInt
+import com.rarible.blockchain.scanner.ethereum.test.data.randomWord
 import com.rarible.blockchain.scanner.monitoring.BlockchainMonitor
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
@@ -14,6 +18,7 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -22,6 +27,7 @@ import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import scalether.core.EthPubSub
 import scalether.core.MonoEthereum
+import scalether.domain.response.Log
 import java.math.BigInteger
 
 @Suppress("ReactiveStreamsUnusedPublisher")
@@ -61,5 +67,38 @@ class EthereumClientTest {
         verify {
             ethereum.ethGetFullBlockByNumber(currentBlock)
         }
+    }
+
+    @Test
+    fun `skip logs from epoch block`() = runBlocking<Unit> {
+        val epochBlock = BigInteger.valueOf(randomInt().toLong())
+        val ethBlock = ethBlock(epochBlock.intValueExact())
+
+        every { properties.ignoreEpochBlocks } returns true
+        every { properties.ignoreNullableLogs } returns false
+        every { ethereum.ethBlockNumber() } returns Mono.just(epochBlock)
+        every { ethereum.ethGetFullBlockByNumber(epochBlock) } returns Mono.just(ethBlock)
+        every { properties.scan } returns ScanProperties(startFromCurrentBlock = true)
+
+        val log = mockk<Log> {
+            every { removed() } returns false
+            every { transactionHash() } returns ethBlock.hash()
+            every { blockHash() } returns ethBlock.hash()
+        }
+        every { ethereum.ethGetLogsJava(any()) } returns Mono.just(listOf(log))
+
+        val descriptor = mockk<EthereumDescriptor> {
+            every { contracts } returns listOf(randomAddress())
+            every { ethTopic } returns randomWord()
+        }
+        val ethereumBlockchainBlock = EthereumBlockchainBlock(ethBlock)
+
+        val logs = ethereumClient.getBlockLogs(
+            descriptor = descriptor,
+            blocks = listOf(ethereumBlockchainBlock),
+            stable = true
+        ).toList()
+
+        assertThat(logs.single().logs).isEmpty()
     }
 }
