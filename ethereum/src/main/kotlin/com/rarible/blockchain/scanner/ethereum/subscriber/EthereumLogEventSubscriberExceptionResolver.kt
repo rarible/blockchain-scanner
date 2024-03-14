e5c5c48cfe216a363d7dd0ba065b01ace7f7aaa5
Igor Baltiyskiy <i.baltiyskiy@rarible.com>
2024-03-14 18:25:36 +0100

    PT-4904 Generalize shouldInterruptScan to WebClientException, share it with other indexers
    The idea is that the classes of exceptions are not specific to Ethereum, and we can encounter them in other indexers as well

--- ethereum/src/main/kotlin/com/rarible/blockchain/scanner/ethereum/subscriber/EthereumLogEventSubscriberExceptionResolver.kt
+++ ethereum/src/main/kotlin/com/rarible/blockchain/scanner/ethereum/subscriber/EthereumLogEventSubscriberExceptionResolver.kt
@@ -1,29 +1,21 @@
 package com.rarible.blockchain.scanner.ethereum.subscriber
 
+import com.mongodb.MongoSocketException
 import com.rarible.blockchain.scanner.framework.subscriber.LogEventSubscriberExceptionResolver
 import io.daonomic.rpc.RpcCodeException
 import org.springframework.dao.DataAccessResourceFailureException
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.client.WebClientResponseException
+import org.springframework.web.reactive.function.client.WebClientException
 import java.io.IOException
 
 @Component
 class EthereumLogEventSubscriberExceptionResolver : LogEventSubscriberExceptionResolver {
-
-    override fun shouldInterruptScan(e: Throwable): Boolean {
-        return isIOException(e) ||
-            isIOException(e.cause) ||
-            isIOException(e.cause?.cause)
-    }
-
-    private fun isIOException(e: Throwable?): Boolean {
-        e ?: return false
-        return when (e) {
-            is RpcCodeException -> true // Node unavailable
-            is WebClientResponseException -> true // Request to external HTTP resource failed
-            is IOException -> true // Request to internal HTTP resource failed, e.g. protocol-currency-api
-            is DataAccessResourceFailureException -> true // Mongo connection error
-            else -> false
-        }
+    override fun isRetriable(e: Throwable): Boolean = when (e) {
+        is RpcCodeException -> true // Node unavailable
+        is WebClientException -> true // Request to external HTTP resource failed
+        is IOException -> true // In general, IOException should be retriable
+        is DataAccessResourceFailureException -> true // Mongo connection error
+        is MongoSocketException -> true // Mongo connection error as well
+        else -> false
     }
 }
--- flow/src/main/kotlin/com/rarible/blockchain/scanner/flow/subscriber/FlowLogEventSubscriberExceptionResolver.kt
+++ flow/src/main/kotlin/com/rarible/blockchain/scanner/flow/subscriber/FlowLogEventSubscriberExceptionResolver.kt
@@ -1,12 +1,21 @@
 package com.rarible.blockchain.scanner.flow.subscriber
 
+import com.mongodb.MongoSocketException
 import com.rarible.blockchain.scanner.framework.subscriber.LogEventSubscriberExceptionResolver
+import org.springframework.dao.DataAccessResourceFailureException
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClientException
+import java.io.IOException
 
 @Component
 class FlowLogEventSubscriberExceptionResolver : LogEventSubscriberExceptionResolver {
 
-    override fun shouldInterruptScan(e: Throwable): Boolean {
-        return false
+    override fun isRetriable(e: Throwable): Boolean = when (e) {
+        is WebClientException -> true // Request to external HTTP resource failed
+        is IOException -> true // Request to internal HTTP resource failed, e.g. protocol-currency-api
+        is DataAccessResourceFailureException -> true // Mongo connection error
+        is MongoSocketException -> true // Mongo connection error as well
+        else -> false
     }
+
 }
