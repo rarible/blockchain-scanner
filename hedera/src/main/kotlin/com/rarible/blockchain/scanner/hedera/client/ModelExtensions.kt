6248614506fdda43a19fe9985c7a9ab950436f56
sanufriev <s.anufriev@rarible.com>
2025-02-04 17:41:45 +0700

    feat: PT-6805 Fix some model error

--- hedera/src/main/kotlin/com/rarible/blockchain/scanner/hedera/client/ModelExtensions.kt
+++ hedera/src/main/kotlin/com/rarible/blockchain/scanner/hedera/client/ModelExtensions.kt
@@ -1,13 +1,14 @@
 package com.rarible.blockchain.scanner.hedera.client
 
 import com.rarible.blockchain.scanner.hedera.client.rest.dto.HederaBlock
+import com.rarible.blockchain.scanner.hedera.utils.consensusTimestampToInstant
 
 fun HederaBlock.toBlockchainBlock(): HederaBlockchainBlock {
     return HederaBlockchainBlock(
         number = number,
         hash = hash,
         parentHash = previousHash,
-        timestamp = timestamp.from.toLong(),
+        timestamp = consensusTimestampToInstant(timestamp.to).epochSecond,
         consensusTimestampFrom = timestamp.from,
         consensusTimestampTo = timestamp.to
     )
