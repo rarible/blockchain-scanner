fdc7e56f2934455e884b97db485c425307b266f2
sanufriev <s.anufriev@rarible.com>
2025-02-04 14:09:03 +0700

    feat: PT-6805 Improve hedera block/log model

--- hedera/src/main/kotlin/com/rarible/blockchain/scanner/hedera/client/HederaBlockchainBlock.kt
+++ hedera/src/main/kotlin/com/rarible/blockchain/scanner/hedera/client/HederaBlockchainBlock.kt
@@ -1,24 +1,17 @@
 package com.rarible.blockchain.scanner.hedera.client
 
 import com.rarible.blockchain.scanner.framework.client.BlockchainBlock
-import com.rarible.blockchain.scanner.hedera.client.rest.dto.HederaBlockDetails
 import java.time.Instant
 
 data class HederaBlockchainBlock(
-    private val block: HederaBlockDetails,
-    override val receivedTime: Instant = Instant.now()
+    override val number: Long,
+    override val hash: String,
+    override val parentHash: String?,
+    override val timestamp: Long,
+    override val receivedTime: Instant = Instant.now(),
+    val consensusTimestampFrom: String,
+    val consensusTimestampTo: String,
 ) : BlockchainBlock {
-    override val number: Long
-        get() = block.number
-
-    override val hash: String
-        get() = block.hash
-
-    override val parentHash: String
-        get() = block.previousHash
-
-    override val timestamp: Long
-        get() = block.timestamp.from.toLong()
 
     override fun withReceivedTime(value: Instant): BlockchainBlock {
         return copy(receivedTime = value)
--- hedera/src/main/kotlin/com/rarible/blockchain/scanner/hedera/client/HederaBlockchainLog.kt
+++ hedera/src/main/kotlin/com/rarible/blockchain/scanner/hedera/client/HederaBlockchainLog.kt
@@ -2,31 +2,9 @@ package com.rarible.blockchain.scanner.hedera.client
 
 import com.rarible.blockchain.scanner.framework.client.BlockchainLog
 import com.rarible.blockchain.scanner.hedera.client.rest.dto.HederaTransaction
-import com.rarible.blockchain.scanner.hedera.client.rest.dto.NftTransfer
-import com.rarible.blockchain.scanner.hedera.client.rest.dto.Transfer
+import com.rarible.blockchain.scanner.hedera.model.HederaLog
 
 class HederaBlockchainLog(
-    private val transaction: HederaTransaction,
-) : BlockchainLog {
-
-    val transactionHash: String
-        get() = transaction.transactionHash
-
-    val entityId: String?
-        get() = transaction.entityId
-
-    val name: String
-        get() = transaction.name
-
-    val consensusTimestamp: String
-        get() = transaction.consensusTimestamp
-
-    val transactionId: String
-        get() = transaction.transactionId
-
-    val transfers: List<Transfer>
-        get() = transaction.transfers
-
-    val nftTransfers: List<NftTransfer>?
-        get() = transaction.nftTransfers
-}
+    val log: HederaLog,
+    val transaction: HederaTransaction,
+) : BlockchainLog
