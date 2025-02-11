0190a8e35a780846f46a8f4005345b5531988bb9
sanufriev <s.anufriev@rarible.com>
2025-02-11 22:12:12 +0700

    feat: PT-6788 Add minor log index

--- hedera/src/main/kotlin/com/rarible/blockchain/scanner/hedera/model/HederaLog.kt
+++ hedera/src/main/kotlin/com/rarible/blockchain/scanner/hedera/model/HederaLog.kt
@@ -1,18 +1,20 @@
 package com.rarible.blockchain.scanner.hedera.model
 
+import com.rarible.blockchain.scanner.hedera.utils.toFixedLengthString
+
 data class HederaLog(
     val blockNumber: Long,
     val blockHash: String,
     val consensusTimestamp: String,
     val transactionHash: String,
     val transactionId: String,
-    val minorLogIndex: Int? = null
+    val minorLogIndex: Int = 0
 ) : Comparable<HederaLog> {
 
     override fun compareTo(other: HederaLog): Int = comparator.compare(this, other)
 
     val stringValue: String
-        get() = consensusTimestamp + if (minorLogIndex != null) ":$minorLogIndex" else ""
+        get() = "$consensusTimestamp:${minorLogIndex.toLong().toFixedLengthString(6)}"
 
     override fun toString(): String = stringValue
 
@@ -20,7 +22,7 @@ data class HederaLog(
         private val comparator =
             compareBy<HederaLog>(
                 { it.consensusTimestamp },
-                { it.minorLogIndex ?: Int.MIN_VALUE },
+                { it.minorLogIndex },
             )
     }
 }
