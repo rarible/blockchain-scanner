8079df6647c5938adc85388a63bc3dbf06d061b1
Igor Baltiyskiy <i.baltiyskiy@rarible.com>
2025-03-15 01:51:34 +0100

    PT-7085 Create reconciliation reindexer directly

--- ethereum/src/main/kotlin/com/rarible/blockchain/scanner/ethereum/EthereumScanner.kt
+++ ethereum/src/main/kotlin/com/rarible/blockchain/scanner/ethereum/EthereumScanner.kt
@@ -10,14 +10,13 @@ import com.rarible.blockchain.scanner.framework.model.TransactionRecord
 import com.rarible.blockchain.scanner.util.subscribeWithRetry
 import kotlinx.coroutines.reactor.mono
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 class EthereumScanner(
-    @Qualifier("ethereumScannerManager") manager: EthereumScannerManager
+    manager: EthereumScannerManager
 ) : BlockchainScanner<EthereumBlockchainBlock, EthereumBlockchainLog, EthereumLogRecord, TransactionRecord, EthereumDescriptor, EthereumLogRepository>(
     manager
 ) {
--- ethereum/src/main/kotlin/com/rarible/blockchain/scanner/ethereum/task/EthereumBlockCheckTaskHandler.kt
+++ ethereum/src/main/kotlin/com/rarible/blockchain/scanner/ethereum/task/EthereumBlockCheckTaskHandler.kt
@@ -8,12 +8,11 @@ import com.rarible.blockchain.scanner.ethereum.model.EthereumLogRecord
 import com.rarible.blockchain.scanner.ethereum.repository.EthereumLogRepository
 import com.rarible.blockchain.scanner.framework.model.TransactionRecord
 import com.rarible.blockchain.scanner.task.BlockCheckTaskHandler
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 
 @Component
 class EthereumBlockCheckTaskHandler(
-    @Qualifier("ethereumScannerManager") manager: EthereumScannerManager
+    manager: EthereumScannerManager
 ) : BlockCheckTaskHandler<EthereumBlockchainBlock, EthereumBlockchainLog, EthereumLogRecord, TransactionRecord, EthereumDescriptor, EthereumLogRepository>(
     manager
 )
